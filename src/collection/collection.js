a4d1f3ffe1917daf0a73f80cd56e2a4b8a32f937
GabeMCM <gabrielmateuscm@gmail.com>
2023-07-25 13:23:54 -0300

    first commit of package

---
+++ src/collection/collection.js
@@ -0,0 +1,72 @@
+import File from '../fs/file.js';
+
+export default class Model {
+	constructor(name, directory) {
+		this.path = `${directory}/${name}.json`;
+		this.name = name;
+		this.file = new File(directory, name);
+	}
+
+  create(data = '{}') {
+    new Promise((resolve, reject) => {
+      
+    })
+  }
+
+	getAll() {
+		const pro = async (resolve, reject) => {
+			try {
+				const data = await this.file.read();
+				resolve(data);
+			} catch (error) {
+				reject(error);
+			}
+		};
+		return new Promise(pro);
+	}
+
+	findAll(obj) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				const data = await this.getAll();
+				let result = {};
+				for (const key in data) {
+					for (const valor in data[key]) {
+						if (obj.hasOwnProperty(valor)) {
+							if (obj[valor] === data[key][valor]) {
+								result[key] = data[key];
+							}
+						}
+					}
+				}
+				resolve(result);
+			} catch (error) {
+				reject(error);
+			}
+		});
+	}
+
+	findExactly(obj) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				const data = await this.getAll();
+				let result = {};
+				for (const key in data) {
+					let match = true;
+					for (const prop in obj) {
+						if (data[key][prop] !== obj[prop]) {
+							match = false;
+							break;
+						}
+					}
+					if (match) {
+						result[key] = data[key];
+					}
+				}
+				resolve(result);
+			} catch (error) {
+				reject(error);
+			}
+		});
+	}
+}
---
+++ src/fs/file.js
@@ -0,0 +1,53 @@
+import fs from "fs";
+
+export default class File {
+	constructor(path, name) {
+		this.direct = `${path}\\${name}.json`;
+	}
+
+	save(data = "[]", encoding = "utf8") {
+		const promisse = (resolve, reject) => {
+			fs.writeFile(
+				this.direct,
+				JSON.stringify(data, null, 2),
+				encoding,
+				(err) => {
+					if (err) {
+						reject(err);
+						return;
+					}
+					resolve(data);
+				}
+			);
+		};
+		return new Promise(promisse);
+	}
+
+	read(encoding = "utf8") {
+		const promisse = (resolve, reject) => {
+			fs.readFile(this.direct, encoding, (err, data) => {
+				if (err) {
+					reject(err);
+					return;
+				}
+				try {
+					const obj = JSON.parse(data);
+					resolve(obj);
+				} catch (error) {
+					reject(error);
+				}
+			});
+		};
+		return new Promise(promisse);
+	}
+
+	update(newData) {
+		const promisse = async (resolve, reject) => {
+			const data = await this.read();
+			const finalData = { ...data, ...newData };
+			await this.save(finalData);
+			resolve(finalData);
+		};
+		return new Promise(promisse);
+	}
+}
